2837735f86d2e6f4fa472d666f058ce3e3dece2e
arko <arkobera77@gmail.com>
2026-08-02 17:27:42 +0530

    fix(backend):Changed to production

--- Backend/main.py
+++ Backend/main.py
@@ -18,9 +18,8 @@ from custom_types import RAGQueryResult, RAGChunkAndSrc, RAGSearchResult, RAGUps
 load_dotenv()
 
 inngest_client = inngest.Inngest(
-    app_id = 'rap_app',
+    app_id = 'rag_app',
     logger = logging.getLogger('uvicorn'),
-    is_production = False,
     serializer = inngest.PydanticSerializer()
 )
 
