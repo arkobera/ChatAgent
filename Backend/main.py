663bb005f454205471a07f670db75f831ab91eea
arko <arkobera77@gmail.com>
2026-08-02 17:55:38 +0530

    updt(backend)

--- Backend/main.py
+++ Backend/main.py
@@ -19,7 +19,7 @@ load_dotenv()
 inngest_client = inngest.Inngest(
     app_id = 'rag_app',
     logger = logging.getLogger('uvicorn'),
-    is_production = False,
+    # is_production = False,
     serializer = inngest.PydanticSerializer()
 )
 
